355a99333b8e3f1a463944758a9313a09baaf0a0
JoseGuzman02 <83370645+JoseGuzman02@users.noreply.github.com>
2022-12-11 21:30:23 -0500

    Create InsertInfo.sql

---
+++ InsertInfo.sql
@@ -0,0 +1,14 @@
+use StudentInfo;
+
+insert students (sfirst, slast, syear, sgrade) values ('Will', 'Smitt', 'SOPH', 87);
+insert students (sfirst, slast, syear, sgrade) values ('Joah', 'Hone', 'FRES', 69);
+insert students (sfirst, slast, syear, sgrade) values ('Carter', 'Lenger', 'FRES', 76);
+insert students (sfirst, slast, syear, sgrade) values ('Joe', 'Simon', 'SEN', 86);
+insert students (sfirst, slast, syear, sgrade) values ('Buka', 'Dikeocha', 'JUN', 99);
+
+insert advisors (afirst, alast) value ('Robet', 'William');
+insert advisors (afirst, alast) value ('Gabee', 'Mcgee');
+
+insert classes (cname, csubject) values ('Geometry', 'Math');
+insert classes (cname, csubject) values ('Algebra', 'Math');
+insert classes (cname, csubject) values ('Painting', 'Art');
